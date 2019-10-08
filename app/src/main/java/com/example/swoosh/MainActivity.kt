291e1dc3308c4f6a5d060ef2ef244ae2c844d62e
noglory <marce.ps666@gmail.com>
2019-10-08 00:52:46 -0400

    First Layout!

--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View){
+        Toast.makeText(applicationContext, "EuchiMano", Toast.LENGTH_SHORT).show()
     }
 }
