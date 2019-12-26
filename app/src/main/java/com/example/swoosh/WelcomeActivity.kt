38edbfa1dabeb77d938c2b78f4eb411d2e3abca2
noglory <marce.ps666@gmail.com>
2019-12-25 23:00:07 -0400

    Intent and Context.

--- app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -1,18 +1,21 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 
-    fun sendMessage(view: View){
-        Toast.makeText(applicationContext, "EuchiMano", Toast.LENGTH_SHORT).show()
-    }
+
 }
